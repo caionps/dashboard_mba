b5e42aaecca372ef8844b1c11f8b2a9c29cab1c9
caionps <caio.nepos@gmail.com>
2024-01-25 15:54:17 -0300

    Atualização dos dados e adição de tabs

--- trabalho_final.py
+++ trabalho_final.py
@@ -1,3 +1,23 @@
+import pandas as pd
 import streamlit as st 
+
+def load_data(path_csv: str) -> pd.DataFrame:
+    with open(path_csv, 'r') as f:
+        df = pd.read_csv(f)
+    return df
+
+def tab_content(df: pd.DataFrame) -> None:
+    col1, col2 = st.columns(2)
+
+
+
+
+
+
+
+
+df_hapvida = load_data('RECLAMEAQUI_HAPVIDA.csv')
+
+
 st.title('Teste')
 
